68bfc745f001744d488f3ec610ebae59845ad2c8
hadley <h.wickham@gmail.com>
2014-10-22 13:44:19 -0500

    Rename vector_smooth to compute_smooth_vec

--- R/smooth-vector.R
+++ R/smooth-vector.R
@@ -17,12 +17,12 @@
 #' x <- runif(1e4, 0, 4 * pi)
 #' y <- sin(x) + runif(1e4, -0.5, 0.5)
 #' plot(x, y)
-#' smu <- vector_smooth(x, y, span = 0.25)
+#' smu <- compute_smooth_vec(x, y, span = 0.25)
 #' lines(smu$x, smu$y, type = "l", col = "red", lwd = 2)
 #' x_grid <- seq(0, 4 * pi, length = 100)
 #' lines(x_grid, sin(x_grid), type = "l", col = "blue", lwd = 2)
 #' lines(x_grid, predict(loess(y ~ x), data.frame(x = x_grid)), col = "green", lwd = 2)
-vector_smooth <- function(x, z, span = 0.25, n_bin = 1000, n_smooth = 100,
+compute_smooth_vec <- function(x, z, span = 0.25, n_bin = 1000, n_smooth = 100,
                           weight = NULL) {
   # Need to remove missing values
 
