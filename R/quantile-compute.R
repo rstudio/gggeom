e6b930f778f910856d40d6e7dd6f6dd83f3560f4
hadley <h.wickham@gmail.com>
2014-10-23 16:09:19 -0500

    Fix typo

--- R/quantile-compute.R
+++ R/quantile-compute.R
@@ -2,6 +2,6 @@ compute_quantile_vec <- function(x, w = NULL, probs = seq(0, 1, 0.25)) {
   if (is.null(w)) {
     quantile(x, probs, na.rm = FALSE, names = FALSE)
   } else {
-    weightedQuantile(x, w, props)
+    weightedQuantile(x, w, probs)
   }
 }
