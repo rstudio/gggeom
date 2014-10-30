bdcc25ae8c0d6e2d1d98f272f1a86bfc71294f0b
hadley <h.wickham@gmail.com>
2014-10-30 11:20:01 -0500

    Add examples to geometry_rotate

--- R/geometry-rotate.R
+++ R/geometry-rotate.R
@@ -5,9 +5,15 @@
 #' @param geom A geometry data frame.
 #' @export
 #' @examples
-#' mtcars %>%
-#'   render_point(~wt, ~mpg) %>%
-#'   geometry_rotate()
+#' scatter <- mtcars %>% render_point(~wt, ~mpg)
+#' scatter %>% plot()
+#' scatter %>% geometry_rotate() %>% plot()
+#'
+#' bar <- mtcars %>%
+#'   compute_count(~cyl) %>%
+#'   render_bar(~x_, ~count_, width = 1)
+#' bar %>% plot()
+#' bar %>% geometry_rotate() %>% plot()
 geometry_rotate <- function(geom) UseMethod("geometry_rotate")
 
 #' @export
@@ -17,7 +23,7 @@ geometry_rotate.geom <- function(geom) {
 
 #' @export
 geometry_rotate.geom_ribbon <- function(geom) {
-  stop("Can't rotate ribbons")
+  stop("Can't rotate ribbons", call. = FALSE)
 }
 
 #' @export
