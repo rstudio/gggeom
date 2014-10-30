51321f3550bb97fb5dbc076b1d68c49241c71402
hadley <h.wickham@gmail.com>
2014-10-30 10:52:31 -0500

    Add base plotting methods for geometries. And fix a few bugs along the way

--- R/render.R
+++ R/render.R
@@ -17,7 +17,7 @@ print.geom <- function(x, ...) {
 #' @param x,y Formulas specifying x and y positions.
 #' @export
 #' @examples
-#' render_point(mtcars, ~mpg, ~wt) %>% plot()
+#' render_point(mtcars, ~mpg, ~wt)
 #' render_path(mtcars, ~mpg, ~wt)
 #' render_polygon(mtcars, ~mpg, ~wt)
 #' render_text(mtcars, ~mpg, ~wt)
@@ -38,7 +38,7 @@ plot.geom_point <- function(x, y, pch = 20, ..., add = FALSE) {
 
 #' @export
 #' @rdname render_text
-render_polygon <- function(data, x, y) {
+render_text <- function(data, x, y) {
   data$x_ <- eval_vector(data, x)
   data$y_ <- eval_vector(data, y)
 
@@ -46,6 +46,12 @@ render_polygon <- function(data, x, y) {
   data
 }
 
+#' @export
+plot.geom_text <- function(x, y, labels = 1:nrow(x), ..., add = FALSE) {
+  if (!add) plot_init(x$x_, x$y_)
+
+  text(x$x_, x$y_, labels = labels, ...)
+}
 
 # Polygon ----------------------------------------------------------------------
 
@@ -59,6 +65,14 @@ render_polygon <- function(data, x, y) {
   data
 }
 
+#' @export
+plot.geom_polygon <- function(x, y, col = "#7F7F7F7F", ..., add = FALSE) {
+  if (!add) plot_init(x$x_, x$y_)
+
+  dplyr::do(x, `_` = polygon(.$x_, .$y_, col = col, ...))
+  invisible()
+}
+
 # Path -------------------------------------------------------------------------
 
 #' @export
@@ -69,6 +83,14 @@ render_path <- function(data, x, y) {
 
   class(data) <- c("geom_path", "geom", class(data))
   data
+}
+
+#' @export
+plot.geom_path <- function(x, y, col = "grey10", ..., add = FALSE) {
+  if (!add) plot_init(x$x_, x$y_)
+
+  dplyr::do(x, `_` = lines(.$x_, .$y_, col = col, ...))
+  invisible()
 }
 
 # Rect -------------------------------------------------------------------------
@@ -92,6 +114,7 @@ render_path <- function(data, x, y) {
 #' mtcars %>%
 #'   compute_count(~cyl) %>%
 #'   render_bar(~x_, ~count_, width = 1)
+#' .Last.value %>% plot()
 render_rect <- function(data, x1, y1, x2, y2) {
   data$x1_ <- eval_vector(data, x1)
   data$x2_ <- eval_vector(data, x2)
@@ -100,6 +123,13 @@ render_rect <- function(data, x1, y1, x2, y2) {
 
   class(data) <- c("geom_rect", "geom", class(data))
   data
+}
+
+#' @export
+plot.geom_rect <- function(x, y, col = "#7F7F7F7F", ..., add = FALSE) {
+  if (!add) plot_init(c(x$x1_, x$x2_), c(x$y1_, x$y2_))
+
+  rect(x$x1_, x$y1_, x$x2_, x$y2_, col = col, ...)
 }
 
 #' @export
@@ -142,6 +172,12 @@ render_tile <- function(data, x, y, width, height, halign = 0.5, valign = 0.5) {
 #' @inheritParams render_point
 #' @param x,y1,y2 x location and y interval.
 #' @export
+#' @examples
+#' x <- 1:10
+#' y <- runif(10, 0, 2)
+#' df <- data.frame(x = x, y1 = x * 2 - y, y2 = x * 2 + y)
+#' render_ribbon(df, ~x, ~y1, ~y2)
+#' .Last.value %>% plot()
 render_ribbon <- function(data, x, y1, y2) {
   data$x_ <- eval_vector(data, x)
   data$y1_ <- eval_vector(data, y1)
@@ -157,6 +193,14 @@ render_area <- function(data, x, y2) {
   render_ribbon(data, x, 0, y2)
 }
 
+#' @export
+plot.geom_ribbon <- function(x, y, col = "#7F7F7F7F", ..., add = FALSE) {
+  if (!add) plot_init(x$x_, c(x$y1_, x$y2_))
+
+  dplyr::do(x, `_` = polygon(c(x$x_, rev(x$x_)), c(x$y1_, rev(x$y2_)), col = col, ...))
+  invisible()
+}
+
 # Arc --------------------------------------------------------------------------
 
 #' Render an arc
@@ -167,7 +211,7 @@ render_area <- function(data, x, y2) {
 #' @param theta1, theta2 Extent of angle (in radians).
 #' @export
 #' @examples
-#' render_arc(mtcars, ~vs, ~am, 0, 5, 0, ~mpg)
+#' render_arc(mtcars, ~vs, ~am, 0, 0.1, 0, ~mpg / max(mpg) * 2 * pi)
 render_arc <- function(data, x, y, r1, r2, theta1, theta2) {
   data$x_ <- eval_vector(data, x)
   data$y_ <- eval_vector(data, y)
@@ -176,6 +220,32 @@ render_arc <- function(data, x, y, r1, r2, theta1, theta2) {
   data$theta1_ <- eval_vector(data, theta1)
   data$theta2_ <- eval_vector(data, theta2)
 
+  class(data) <- c("geom_arc", "geom", class(data))
   data
 }
 
+#' @export
+plot.geom_arc <- function(x, y, ..., col = "#7F7F7F7F", add = FALSE) {
+  x$id_ <- 1:nrow(x)
+  polys <- x %>%
+    dplyr::group_by(id_) %>%
+    dplyr::do(
+      make_arc(.$x_, .$y_, c(.$r1_, .$r2_), c(.$theta1_, .$theta2_))
+    )
+
+  if (!add) plot_init(polys$x_, polys$y_)
+  dplyr::do(polys, `_` = polygon(.$x, .$y, col = col, ...))
+  invisible()
+}
+
+make_arc <- function(x, y, r, theta) {
+  inner_theta <- seq(theta[1], theta[2], length = 1 + (r[1] * 2 * pi) / 0.01)
+  inner_x <- x + r[1] * cos(inner_theta)
+  inner_y <- y + r[1] * sin(inner_theta)
+
+  outer_theta <- seq(theta[1], theta[2], length = 1 + (r[2] * 2 * pi) / 0.01)
+  outer_x <- x + r[2] * cos(rev(outer_theta))
+  outer_y <- y + r[2] * sin(rev(outer_theta))
+
+  data.frame(x_ = c(inner_x, outer_x), y_ = c(inner_y, outer_y))
+}
