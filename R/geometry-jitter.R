41640fabbde7ed8721c768a67453b84fc5e53bb1
hadley <h.wickham@gmail.com>
2014-11-04 15:55:39 -0600

    Fix jittering bugs

--- R/geometry-jitter.R
+++ R/geometry-jitter.R
@@ -42,8 +42,8 @@ geometry_jitter.geom_ribbon <- function(geom, x = 0, y = resolution(geom$y1_) * 
   geom$x_ <- geom$x_ + jitter(geom, x)
 
   y_jitter <- jitter(geom, y)
-  geom$y1_ <- geom$y1_ + y_jitter()
-  geom$y2_ <- geom$y2_ + y_jitter()
+  geom$y1_ <- geom$y1_ + y_jitter
+  geom$y2_ <- geom$y2_ + y_jitter
 
   geom
 }
@@ -53,12 +53,12 @@ geometry_jitter.geom_rect <- function(geom,
                                       x = resolution(geom$x1_) * 0.4,
                                       y = resolution(geom$y1_) * 0.4) {
   x_jitter <- jitter(geom, x)
-  geom$x1_ <- geom$x1_ + x_jitter()
-  geom$x2_ <- geom$x2_ + x_jitter()
+  geom$x1_ <- geom$x1_ + x_jitter
+  geom$x2_ <- geom$x2_ + x_jitter
 
   y_jitter <- jitter(geom, y)
-  geom$y1_ <- geom$y1_ + y_jitter()
-  geom$y2_ <- geom$y2_ + y_jitter()
+  geom$y1_ <- geom$y1_ + y_jitter
+  geom$y2_ <- geom$y2_ + y_jitter
 
   geom
 }
