a8ad60d06a623392d31a8348b67d91effe223212
hadley <h.wickham@gmail.com>
2014-11-04 14:38:21 -0600

    Only include relevant cols in sample data

--- data-raw/examples.R
+++ data-raw/examples.R
@@ -1,20 +1,20 @@
 library(dplyr)
 data("mpg", package = "ggplot2")
 
-scatter_ex <- render_point(mpg, ~cty, ~hwy)
+scatter_ex <- render_point(mpg, ~cty, ~hwy) %>% select(ends_with("_"))
 scatter_ex %>% plot()
 scatter_ex %>% geometry_jitter() %>% plot(col = "red")
 scatter_ex %>% geometry_rotate() %>% plot()
 scatter_ex %>% geometry_flip() %>% plot()
-devtools::use_data(scatter_ex)
+devtools::use_data(scatter_ex, overwrite = TRUE)
 
-bar_ex <- render_bar(mpg, ~cyl, 1)
+bar_ex <- render_bar(mpg, ~cyl, 1) %>% select(ends_with("_"))
 bar_ex %>% plot()
 bar_ex %>% geometry_stack() %>% plot()
 bar_ex %>% geometry_stack() %>% geometry_rotate() %>% plot()
 bar_ex %>% geometry_rotate() %>% geometry_stack("x") %>% plot()
 
-devtools::use_data(bar_ex)
+devtools::use_data(bar_ex, overwrite = TRUE)
 
 
 histogram_ex <- mpg %>%
@@ -22,4 +22,4 @@ histogram_ex <- mpg %>%
   render_rect(~xmin_, 0, ~xmax_, ~count_)
 histogram_ex %>% plot()
 histogram_ex %>% geometry_flip("y") %>% plot()
-devtools::use_data(histogram_ex)
+devtools::use_data(histogram_ex, overwrite = TRUE)
