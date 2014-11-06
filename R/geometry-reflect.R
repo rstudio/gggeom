60ca1a4f61e976a96bcbed0bddbea4a5c7b29f97
Winston Chang <winston@stdout.org>
2014-11-06 13:28:42 -0600

    Rename geometry_flip to geometry_reflect

---
+++ R/geometry-reflect.R
@@ -0,0 +1,69 @@
+#' Reflect positions around an axis.
+#'
+#' @inheritParams geometry_rotate
+#' @param dir Direction in which to reflect. One of "x" and "y". (Specifying "x"
+#'   will flip the x values about the y axis.
+#' @export
+#' @examples
+#' scatter_ex %>% plot()
+#' scatter_ex %>% geometry_reflect() %>% plot()
+#' scatter_ex %>% geometry_reflect("y") %>% plot()
+#'
+#' histogram_ex %>% plot()
+#' histogram_ex %>% geometry_reflect() %>% plot()
+#' histogram_ex %>% geometry_reflect("y") %>% plot()
+#' histogram_ex %>% geometry_reflect("y") %>% geometry_rotate() %>% plot()
+geometry_reflect <- function(geom, dir = c("x", "y")) {
+  UseMethod("geometry_reflect")
+}
+
+#' @export
+geometry_reflect.geom <- function(geom, dir = c("x", "y")) {
+  dir <- match.arg(dir)
+
+  if (dir == "x") {
+    geom$x_ <- -geom$x_
+  } else {
+    geom$y_ <- -geom$y_
+  }
+
+  geom
+}
+
+#' @export
+geometry_reflect.geom_ribbon <- function(geom, dir = c("x", "y")) {
+  dir <- match.arg(dir)
+
+  if (dir == "x") {
+    geom$x1_ <- -geom$x1_
+    geom$x2_ <- -geom$x2_
+    geom <- switch_cols(geom, "x1_", "x2")
+  } else {
+    geom$y_ <- -geom$y
+  }
+
+  geom
+}
+
+#' @export
+geometry_reflect.geom_rect <- function(geom, dir = c("x", "y")) {
+  dir <- match.arg(dir)
+
+  if (dir == "x") {
+    geom$x1_ <- -geom$x1_
+    geom$x2_ <- -geom$x2_
+    geom <- switch_cols(geom, "x1_", "x2_")
+  } else {
+    geom$y1_ <- -geom$y1_
+    geom$y2_ <- -geom$y2_
+    geom <- switch_cols(geom, "y1_", "y2_")
+  }
+
+  geom
+}
+
+#' @export
+geometry_reflect.geom_segment <- function(geom, dir = c("x", "y")) {
+  geometry_reflect.geom_rect(geom, dir)
+}
+
--- data-raw/examples.R
+++ data-raw/examples.R
@@ -5,7 +5,7 @@ scatter_ex <- render_point(mpg, ~cty, ~hwy) %>% select(ends_with("_"))
 scatter_ex %>% plot()
 scatter_ex %>% geometry_jitter() %>% plot(col = "red")
 scatter_ex %>% geometry_rotate() %>% plot()
-scatter_ex %>% geometry_flip() %>% plot()
+scatter_ex %>% geometry_reflect() %>% plot()
 devtools::use_data(scatter_ex, overwrite = TRUE)
 
 bar_ex <- render_bar(mpg, ~cyl, 1) %>% select(ends_with("_"))
@@ -21,5 +21,5 @@ histogram_ex <- mpg %>%
   compute_bin(~hwy, width = 1) %>%
   render_rect(~xmin_, 0, ~xmax_, ~count_)
 histogram_ex %>% plot()
-histogram_ex %>% geometry_flip("y") %>% plot()
+histogram_ex %>% geometry_reflect("y") %>% plot()
 devtools::use_data(histogram_ex, overwrite = TRUE)
