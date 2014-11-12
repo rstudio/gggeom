e252fcb7f6adc0c6415de98109fa930baccd6c7b
hadley <h.wickham@gmail.com>
2014-11-12 17:04:12 -0600

    Crazy example.

--- R/warp.R
+++ R/warp.R
@@ -20,6 +20,14 @@
 #'
 #' path %>% geometry_warp("polar") %>% plot()
 #' path %>% geometry_warp("polar", tolerance = 0.00001) %>% plot() %>% points()
+#'
+#' # Crazy example
+#' expand.grid(x = seq(0, pi, length = 4), y = 1:3) %>%
+#'   render_tile(~x, ~y, halign = 0, valign = 0) %>%
+#'   geometry_pointificate() %>%
+#'   geometry_rotate(10) %>%
+#'   geometry_warp("polar", tolerance = 0.0001) %>%
+#'   plot()
 geometry_warp <- function(geom, fun = c("polar", "identity"), tolerance = 0.001) {
   UseMethod("geometry_warp")
 }
