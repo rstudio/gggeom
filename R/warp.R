482cd75a871d4798e516b1a8397ec3c71c7e4c9e
hadley <h.wickham@gmail.com>
2014-11-12 17:21:44 -0600

    Some basic (& incorrect) code to guess reasonable tolerance.

--- R/warp.R
+++ R/warp.R
@@ -8,7 +8,7 @@
 #' @param fun A warping function to use. Currently these are hard coded
 #'   because they much be implemented in C++ for performance reasons.
 #' @param tolerance Approximation errors below this threshold will be
-#'   ignored.
+#'   ignored. If \code{NULL}, attempts to guess a reasonable value.
 #' @export
 #' @examples
 #' spiral <- data.frame(
@@ -19,24 +19,25 @@
 #' path %>% plot() %>% points()
 #'
 #' path %>% geometry_warp("polar") %>% plot()
-#' path %>% geometry_warp("polar", tolerance = 0.00001) %>% plot() %>% points()
+#' path %>% geometry_warp("polar", tolerance = 0.1) %>% plot() %>% points()
 #'
 #' # Crazy example
-#' expand.grid(x = seq(0, pi, length = 4), y = 1:3) %>%
+#' expand.grid(x = seq(0, pi, length = 4), y = 0:3) %>%
 #'   render_tile(~x, ~y, halign = 0, valign = 0) %>%
 #'   geometry_pointificate() %>%
-#'   geometry_rotate(10) %>%
-#'   geometry_warp("polar", tolerance = 0.0001) %>%
+#'   geometry_rotate(15) %>%
+#'   geometry_warp("polar") %>%
 #'   plot()
-geometry_warp <- function(geom, fun = c("polar", "identity"), tolerance = 0.001) {
+geometry_warp <- function(geom, fun = c("polar", "identity"), tolerance = NULL) {
   UseMethod("geometry_warp")
 }
 
 #' @export
 geometry_warp.geom_path <- function(geom, fun = c("polar", "identity"),
-                                    tolerance = 0.001) {
+                                    tolerance = NULL) {
+  tolerance <- tolerance %||% guess_tolerance(geom$x_, geom$y_)
 
-  warped <- Map(function(x, y) warp(x, y, fun, tolerance), geom$x_, geom$y_)
+  warped <- Map(function(x, y) warp(x, y, fun, tolerance ^ 2), geom$x_, geom$y_)
   geom$x_ <- coords(pluck(warped, "x"))
   geom$y_ <- coords(pluck(warped, "y"))
 
@@ -45,12 +46,24 @@ geometry_warp.geom_path <- function(geom, fun = c("polar", "identity"),
 
 #' @export
 geometry_warp.geom_polygon <- function(geom, fun = c("polar", "identity"),
-                                       tolerance = 0.001) {
+                                       tolerance = NULL) {
 
-  warped <- Map(function(x, y) warp(x, y, fun, tolerance, closed = TRUE),
+  tolerance <- tolerance %||% guess_tolerance(geom$x_, geom$y_)
+
+  warped <- Map(function(x, y) warp(x, y, fun, tolerance ^ 2, closed = TRUE),
     geom$x_, geom$y_)
   geom$x_ <- coords(pluck(warped, "x"))
   geom$y_ <- coords(pluck(warped, "y"))
 
   geom
+}
+
+guess_tolerance <- function(x, y) {
+  # Need to be doing this on transformed range, not original!
+  x_rng <- diff(range(x, na.rm = TRUE))
+  y_rng <- diff(range(y, na.rm = TRUE))
+
+  guess <- min(x_rng, y_rng) / 1e3
+  message("Using tolerance of ", format(guess, digits = 3))
+  guess
 }
